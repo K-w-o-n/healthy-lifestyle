a0e46940129c4e719c0a158a0c49f7e7681749d5
Kwon <144373232+K-w-o-n@users.noreply.github.com>
2024-04-16 16:17:34 +0630

    remove nav

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { Routes, Route,} from "react-router-dom";
 import SignIn from './Components/SignIn.jsx'
 import Register from './Components/Register.jsx'
-import Navbar from './Components/Navbar.jsx'
+// import Navbar from './Components/Navbar.jsx'
+import p1 from "../../healthy-lifestyle/src/assets/images/hl.jpg"
+import ex from "../../healthy-lifestyle/src/assets/images/ex.jpg"
+import ex1 from "../../healthy-lifestyle/src/assets/images/ex1.png";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <div className="flex w-full h-screen">
+      
+      <div className="flex max-w-[80] h-screen">
+        <div className="hidden relative h-full w-1/2 md:flex">
+          <img src={ex} alt="" className="w-full h-full object-cover" />
+        </div>
         <div className="w-full flex  items-center justify-center lg:w-1/2">
           <Routes>
             <Route path="/signin" element={<SignIn />} />
